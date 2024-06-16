183e6a3933ce4277ded0228d87cd0c648173c83b
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-16 23:59:32 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UserEntryComponent } from './user-entry/user-entry.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    UserEntryComponent,
+    UserListComponent,
+    UserAddComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dr-settings',
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+
+}
