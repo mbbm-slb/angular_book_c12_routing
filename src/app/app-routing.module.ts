86313777ad5aba60f98f2369b57a42afaac655cb
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-17 00:10:26 +0200

    demo: flat routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SettingsComponent } from './settings/settings.component';
+import { UserEntryComponent } from './user-entry/user-entry.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+
+
+const routes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: 'user-entry', component: UserEntryComponent },
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-add', component: UserAddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
