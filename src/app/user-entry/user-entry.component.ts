3936cba9c6747eea0fba9ea2de5a74e9312977a3
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-19 23:24:51 +0200

    Route programmatisch wechseln

--- src/app/user-entry/user-entry.component.ts
+++ src/app/user-entry/user-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dr-user-entry',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './user-entry.component.css'
 })
 export class UserEntryComponent {
+  constructor(private router: Router) {}
+
+  navigateToAddUser() {
+    this.router.navigate(['/user-entry/user-add']);
+    
+    //alternative ways to navigate
+    //this.router.navigate(['/user-entry', 'user-add']);
+    //this.router.navigateByUrl('/user-entry/user-add');
+  }
+
 
 }
